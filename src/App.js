c0cd1487f6e4c40b1e3f84dadcbd6a957b13557c
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-09 18:45:03 -0500

    Loading page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import LoadingPage from "./Components/LoadingPage/LoadingPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <LoadingPage />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
