d16359727a830d5e9351a9c3cc8ba9daa2d28f06
rozan Karimi <rozan.karimii@gmail.com>
2024-02-08 19:36:55 -0500

    Header Component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+import "./_Header.scss";
+import siteLogo from "../../assets/Fiserv-logo-1024x577-1451217154.png";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App--header">
+        {/* <link to="/"> */}
+        <img src={siteLogo} alt="Fiserv Logo" className="App--logo" />
+        {/* </link> */}
+        <div className="App--SubHeader">
+          <div className="App--SubHeader__left">
+            <h3 className="Hero__text">Thank you for dining with us</h3>
+          </div>
+          <div className="App--SubHeader__right">
+            <h4>3 Guests</h4>
+          </div>
+        </div>
+        <button type="text" className="App--SubHeader__Button">
+          {" "}
+          Request your server, Jack
+        </button>
+      </div>
+    );
+  }
+}
+export default Header;
