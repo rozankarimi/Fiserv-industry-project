adc0438b624d30886884f35d250af440f14437d5
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-10 00:25:06 -0500

    commented code removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
-// import "./App.css";
+import "./App.css";
 // import DropDownOrder from "./Components/DropDownOrder/DropDownOrder";
 // import LoadingPage from "./Components/LoadingPage/LoadingPage";
 import PaymentMethodButton from "./Components/PaymentMethodButton/PaymentMethodButton";
 function App() {
   return (
     <div className="App">
-      <PaymentMethodButton />
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
