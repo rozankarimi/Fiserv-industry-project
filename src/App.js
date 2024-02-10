0cfc7f616da530e374f49777ea8fc619b5dd3896
TonySMK <tonysun25light@live.ca>
2024-02-10 18:40:21 -0500

    added the route for PaymentSuccessPage in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage/HomePage";
 import PaymentPage from "./Pages/PaymentPage/PaymentPage";
+import PaymentSuccessPage from "./Pages/PaymentSuccessPage/PaymentSuccessPage";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/paymentPage" element={<PaymentPage />} />
+          <Route path="/paymentSuccessPage" element={<PaymentSuccessPage />} />
         </Routes>
       </BrowserRouter>
     </div>
