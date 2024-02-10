d0889d7286fa6498d859cb109d06f238c2a7b565
TonySMK <tonysun25light@live.ca>
2024-02-10 18:32:57 -0500

    updates...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Header from "./Components/Header/Header";
+import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage/HomePage";
 import PaymentPage from "./Pages/PaymentPage/PaymentPage";
-import Ham from "./Pages/PaymentSuccessPage/PaymentSuccessPage";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Header /> */}
-        {/* <Routes>
+        <Header />
+        <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/paymentPage" element={<PaymentPage />} />
-        </Routes> */}
+        </Routes>
       </BrowserRouter>
-      <Ham />
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./_Header.scss";
 import siteLogo from "../../assets/Fiserv-logo-1024x577-1451217154.png";
 
