8406a83420d2c7c40deae659cf186fcbdbff9023
TonySMK <tonysun25light@live.ca>
2024-02-09 20:29:38 -0500

    updated button style added font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
+import Dahaha from "./component/paypaybutton/paypaybutton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Dahaha price={12.12} />
     </div>
   );
 }
