acc1c2c7e21a264204d498b8cf6d5310daef7cd1
rozan Karimi <rozan.karimii@gmail.com>
2024-02-10 15:46:44 -0500

    Header needs to be reviewed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 // import "./App.css";
 // import DropDownOrder from "./Components/DropDownOrder/DropDownOrder";
-import LoadingPage from "./Components/LoadingPage/LoadingPage";
-import PaymentMethodButton from "./Components/PaymentMethodButton/PaymentMethodButton";
+// import LoadingPage from "./Components/LoadingPage/LoadingPage";
+import Header from "./Components/Header/Header";
+// import PaymentMethodButton from "./Components/PaymentMethodButton/PaymentMethodButton";
+
 function App() {
   return (
     <div className="App">
-
+      {/* <LoadingPage /> */}
+      <Header />
+      {/* <PaymentMethodButton /> */}
     </div>
   );
 }
