0ef90e78aa93d4dc1bf61b2619cf5f77937ca29c
TonySMK <tonysun25light@live.ca>
2024-02-10 12:38:13 -0500

    update ui

--- src/Components/PopupElement/PopupElement.js
+++ src/Components/PopupElement/PopupElement.js
@@ -6,19 +6,33 @@ export default function () {
       <div className="topbar"></div>
       <div className="formtitle">Add a Debit or Credit Card</div>
       <form className="payment">
-        <label className="payment_namelabel">Name on Card</label>
-        <input className="payment_nameinput" name="name" type="text" />
-        <label className="payment_addresslabel">Address</label>
-        <input className="payment_addressinput" name="address" type="text" />
-        <div>
-          <label className="payment_citylabel">City</label>
-          <input className="payment_cityinput" name="city" type="text" />
-          <label className="payment_statelabel">State</label>
-          <input className="payment_stateinput" name="state" type="text" />
-          <label className="payment_zipcodelabel">Zipcode</label>
-          <input className="payment_zipcodeinput" name="zipcode" type="text" />
+        <div className="section1 am">
+          <label className="section1_wnamelabel">Name on Card</label>
+          <input className="section1_nameinput" name="name" type="text" />
+        </div>
+        <div className="section2 am">
+          <label className="section2_addresslabel">Address</label>
+          <input className="section2_addressinput" name="address" type="text" />
         </div>
 
+        <div className="payment__formbottom">
+          <div className="section3 am">
+            <label className="payment_citylabel">City</label>
+            <input className="payment_cityinput" name="city" type="text" />
+          </div>
+          <div className="section4 am">
+            <label className="payment_statelabel">State</label>
+            <input className="payment_stateinput" name="state" type="text" />
+          </div>
+          <div className="section5 am">
+            <label className="payment_zipcodelabel">Zipcode</label>
+            <input
+              className="payment_zipcodeinput"
+              name="zipcode"
+              type="text"
+            />
+          </div>
+        </div>
         <button className="payment_cardbutton">Add Card</button>
       </form>
     </div>
