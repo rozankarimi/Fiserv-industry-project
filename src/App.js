b6f7d87812462f63c5f992208cf9cd1d5bf7835d
TonySMK <tonysun25light@live.ca>
2024-02-09 20:12:05 -0500

    Made general pay button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
-// import "./App.css";
-import PayPayButton from "./Component/paypaybutton/paypaybutton";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <PayPayButton price={12.23} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
