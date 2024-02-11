c780a0a9ce570ae8fdbca37f58d8062f59e0fcf2
TonySMK <tonysun25light@live.ca>
2024-02-11 16:32:25 -0500

    Update...

--- src/Pages/PaymentFormStep1Page/PaymentFormPage1.js
+++ src/Pages/PaymentFormStep1Page/PaymentFormPage1.js
@@ -3,7 +3,7 @@ import Footer from "../../Components/Footer/Footer";
 import PayWithButton from "../../Components/PayWithButton/PayWithButton";
 import ComfirmPayment from "../../Components/GeneralButton/GeneralButton";
 
-export default function PaymentOpPay1({ payMethod, price, seatvalue }) {
+export default function PaymentOpPay1({ seatvalue }) {
   payMethod = "Seat";
   price = 39.82;
   seatvalue = 1;
@@ -14,25 +14,10 @@ export default function PaymentOpPay1({ payMethod, price, seatvalue }) {
         <div className="headerbuttonwrap">placholder0-button</div>
       </div>
       <div className="formstep2__body">
-        <div className="top">
-          <div className="top__titlewrap">Pay by {payMethod}</div>
-          <div className="top__progresswrap">placeholder1-progressbar</div>
-          <div className="top__pricewrap">${price}</div>
-          <div className="top__messagewrap">
-            For Uni Suchi - Seat {seatvalue}
-          </div>
-          <div className="middle">
-            <PayWithButton method="Debit or Credit" text="some" />
-            <PayWithButton method="Zelle" text="somehum" />
-            <PayWithButton method="Apple Pay" text="somehumbum" />
-          </div>
-        </div>
+        <div className="top"></div>
 
         <div className="bottom">
-          <ComfirmPayment
-            text="Confirm Payment"
-            buttonclass="comfirmpaymentbutton"
-          />
+          <ComfirmPayment text="start" buttonclass="comfirmpaymentbutton" />
         </div>
       </div>
 
