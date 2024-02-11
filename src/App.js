64b70c27924d24d13287c78f05b089cee3b5bea6
TonySMK <tonysun25light@live.ca>
2024-02-10 19:03:47 -0500

    Initiated Payment Options Page 1 -this is not Payment Options Page 0

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage/HomePage";
 import PaymentPage from "./Pages/PaymentPage/PaymentPage";
+import Pay1 from "./Pages/PaymentOpPay1Page/PaymentOpPay1Page";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/paymentPage" element={<PaymentPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+
+      <Pay1 />
     </div>
   );
 }
---
+++ src/Pages/PaymentOpPay1Page/PaymentOpPay1Page.js
@@ -0,0 +1,5 @@
+import "./PaymentOpPay1Styles.scss";
+
+export default function PaymentOpPay1() {
+  return <div className="paymentoppay1"></div>;
+}
