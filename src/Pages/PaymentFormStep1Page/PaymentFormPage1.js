2f4b36f525009914608483392e51e39ca4e4781e
TonySMK <tonysun25light@live.ca>
2024-02-11 17:17:30 -0500

    Updata body...

--- src/Pages/PaymentFormStep1Page/PaymentFormPage1.js
+++ src/Pages/PaymentFormStep1Page/PaymentFormPage1.js
@@ -4,17 +4,17 @@ import PayWithButton from "../../Components/PayWithButton/PayWithButton";
 import ComfirmPayment from "../../Components/GeneralButton/GeneralButton";
 
 export default function PaymentOpPay1({ seatvalue }) {
-  payMethod = "Seat";
-  price = 39.82;
-  seatvalue = 1;
-
   return (
     <div className="formstep2">
       <div className="formstep2__header">
         <div className="headerbuttonwrap">placholder0-button</div>
       </div>
       <div className="formstep2__body">
-        <div className="top"></div>
+        <div className="top">
+          <div className="top__titlewrap">Payment Options</div>
+          <div className="top__progresswrap">placeholder1-progressbar</div>
+          <div className="top__paymentmeans">placeholder2-progressbar</div>
+        </div>
 
         <div className="bottom">
           <ComfirmPayment text="start" buttonclass="comfirmpaymentbutton" />
